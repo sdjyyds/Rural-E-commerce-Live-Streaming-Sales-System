9d8f7de4a37a0539dd259be8d3042abab7115eb3
sdjyyds <3402243478@qq.com>
2025-06-01 12:08:04 +0800

    first-commit

---
+++ src/main/java/com/sdjyyds/system/RuralECommerceLiveStreamingSalesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sdjyyds.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuralECommerceLiveStreamingSalesSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuralECommerceLiveStreamingSalesSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdjyyds/system/RuralECommerceLiveStreamingSalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdjyyds.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralECommerceLiveStreamingSalesSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
